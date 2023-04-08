5e73bdf58b0b7aade11c0005e5df96745775a3a0
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-04-08 15:32:00 +0300

    feat: define and connect to database

--- index.js
+++ index.js
@@ -5,16 +5,35 @@ const multer = require("multer");
 
 // Internal Imports
 const database = require("./util/database");
+
+//Models Imports
+const User = require("./models/user");
+const Task = require("./models/task");
+
 const app = express();
 const upload = multer();
 app.use(express.json);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
-try {
-  database.authenticate();
-  console.log("ok");
-} catch (e) {
-  console.error(e);
-}
 
-app.listen(3000);
+// Database Relations
+User.hasMany(Task);
+Task.belongsTo(User, {
+  constrains: true,
+});
+database
+  .sync()
+  .then((result) => {
+    database
+      .authenticate()
+      .then((result) => {
+        app.listen(3000);
+        console.log("server now is working :  ", result);
+      })
+      .catch((reason) => {
+        throw reason;
+      });
+  })
+  .catch((reason) => {
+    console.error("something went wrong  ", reason);
+  });
