7bb8ee73de4adbec87a3c28bd09fda508b5cb998
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-04-08 14:58:08 +0300

    feat: enable multer and body-praser to extract data from requests

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+//External Imports
 const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+
+// Internal Imports
 const database = require("./util/database");
 const app = express();
-
+const upload = multer();
+app.use(express.json);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
 try {
   database.authenticate();
   console.log("ok");
 } catch (e) {
   console.error(e);
 }
-app.use((request, response, next) => {});
 
 app.listen(3000);
