d5a126071ea95265539c2826edaf4665eb0e9191
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-04-08 14:50:37 +0300

    feat: open database using sequlize

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const database = require("./util/database");
+const app = express();
 
+try {
+  database.authenticate();
+  console.log("ok");
+} catch (e) {
+  console.error(e);
+}
+app.use((request, response, next) => {});
+
+app.listen(3000);
