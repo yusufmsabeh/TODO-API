868d165badfc161b96e56cf7632d35e5d6866453
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-04-09 10:57:14 +0300

    feat: return a message when waking up the replit

--- controllers/keep_alive.js
+++ controllers/keep_alive.js
@@ -1,6 +1,7 @@
 exports.getKeepAlive = async (request, response, next) => {
   try {
     response.status(200).send("I am A live");
+    console.log("I am A live");
   } catch (error) {
     console.log(error);
     response.sendStatus(500);
