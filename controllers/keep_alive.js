f55f60d6ae98897aed4beacfc1aa9b35889ab307
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-04-10 17:57:23 +0300

    feat: display time

--- controllers/keep_alive.js
+++ controllers/keep_alive.js
@@ -1,7 +1,7 @@
 exports.getKeepAlive = async (request, response, next) => {
   try {
     response.status(200).send("I am A live");
-    console.log("I am A live");
+    console.log("I am A live  ", new Date());
   } catch (error) {
     console.error(error);
     response.sendStatus(500);
