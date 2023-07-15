d80aef973e15dbf1d9ee07d6cb9ba642e2ae38e1
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-15 19:09:55 +0300

    refactor: globalize port

--- index.js
+++ index.js
@@ -12,36 +12,38 @@ const keepAliveRouter = require("./routes/keep_alive");
 //Models Imports
 const User = require("./models/user");
 const Task = require("./models/task");
-const { DataTypes } = require("sequelize");
+const {DataTypes} = require("sequelize");
 dotenv.config();
+const PORT = process.env.PORT ?? 3000;
 const app = express();
 const upload = multer();
 app.use(keepAliveRouter);
 app.use((request, response, next) => {
-  console.log("request");
-  next();
+    console.log("request");
+    next();
 });
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(upload.array());
 app.use("/auth", authRouter);
 app.use(userRouter);
-User.hasMany(Task, { foreignKey: { allowNull: false } });
+User.hasMany(Task, {foreignKey: {allowNull: false}});
 
 Task.belongsTo(User);
 database
-  .sync()
-  .then((result) => {
-    database
-      .authenticate()
-      .then((result) => {
-        app.listen(3000);
-        console.log("server now is working :  ", result);
-      })
-      .catch((reason) => {
-        throw reason;
-      });
-  })
-  .catch((reason) => {
-    console.error("something went wrong  ", reason);
-  });
+    .sync()
+    .then((result) => {
+        database
+            .authenticate()
+            .then((result) => {
+                app.listen(PORT, () => {
+                    console.log("server now is working on :", PORT)
+                });
+            })
+            .catch((reason) => {
+                throw reason;
+            });
+    })
+    .catch((reason) => {
+        console.error("something went wrong  ", reason);
+    });
